0518eccbea002d4b7bb04704113e466d621f9578
Daniel Albino <daniel.albino@gmail.com>
2024-08-05 13:30:17 -0300

    starting project.

---
+++ src/test/java/com/azusah/sales_api/SalesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azusah.sales_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
