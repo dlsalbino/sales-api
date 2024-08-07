f622d4af58e299bff08db3a2d6e1b4de1e7665dc
Daniel Albino <daniel.albino@gmail.com>
2024-08-07 15:35:23 -0300

    implementing controller for retrieve product flow.

--- product-api/src/main/java/com/azusah/infrastructure/controller/ProductController.java
+++ product-api/src/main/java/com/azusah/infrastructure/controller/ProductController.java
@@ -5,23 +5,24 @@ import com.azusah.infrastructure.controller.payload.request.ProductRequest;
 import com.azusah.infrastructure.controller.payload.response.ProductResponse;
 import com.azusah.infrastructure.mapper.ProductMapper;
 import com.azusah.usecase.CreateProductUseCase;
+import com.azusah.usecase.RetrieveProductUseCase;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/v1/products")
 public class ProductController {
 
     private final CreateProductUseCase createProductUseCase;
+    private final RetrieveProductUseCase retrieveProductUseCase;
     private final ProductMapper mapper;
 
-    public ProductController(CreateProductUseCase createProductUseCase, ProductMapper mapper) {
+    public ProductController(CreateProductUseCase createProductUseCase, RetrieveProductUseCase retrieveProductUseCase,
+                             ProductMapper mapper) {
         this.createProductUseCase = createProductUseCase;
+        this.retrieveProductUseCase = retrieveProductUseCase;
         this.mapper = mapper;
     }
 
@@ -30,5 +31,12 @@ public class ProductController {
         Product product = createProductUseCase.execute(mapper.toProductDomainFrom(productRequest));
         ProductResponse response = mapper.toProductResponseFrom(product);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> retrieve(@PathVariable Long id) {
+        Product product = retrieveProductUseCase.execute(id);
+        ProductResponse response = mapper.toProductResponseFrom(product);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
